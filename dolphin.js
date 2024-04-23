4337a24a83b496c53e42a83f0988b02ff79cd6e7
Paignn <99561222+Paignn@users.noreply.github.com>
2024-04-23 12:23:03 +0700

    Add files via upload

---
+++ dolphin.js
@@ -0,0 +1,71 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+// import { FBXLoader } from 'three/addons/loaders/FBXLoader.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import Stats from 'three/addons/libs/stats.module.js'
+
+const scene = new THREE.Scene()
+scene.background = new THREE.Color( 0x98F5F9 );
+scene.add(new THREE.AxesHelper(5))
+
+const light = new THREE.PointLight(0xffffff, 10)
+light.position.set(1, 1, 1.0)
+scene.add(light)
+
+const ambientLight = new THREE.AmbientLight()
+scene.add(ambientLight)
+
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+camera.position.set(10, 10, 10)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.target.set(0, 1, 0)
+
+let orca;
+const loader = new GLTFLoader()
+loader.load('models/dolphin/scene.gltf', function (gltf) {
+        orca = gltf.scene
+        orca.scale.set(0.1, 0.1, 0.1)
+        scene.add(orca)
+    }
+)
+
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
+
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+function animate() {
+    requestAnimationFrame(animate)
+    if (orca) {
+       orca.rotation.x += 0.01
+       orca.rotation.y += 0.01
+       orca.rotation.z += 0.01
+    }
+    controls.update()
+    render()
+    stats.update()
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
